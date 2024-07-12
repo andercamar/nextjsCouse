5dacd0b0a30078e65bfa6887a2cfa2a2e19004b0
Anderson <andercamar@gmail.com>
2024-07-11 23:21:53 -0300

    add cn helper

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,22 +1,29 @@
 import { HomeIcon, GamepadIcon, AwardIcon, MapIcon, GhostIcon } from "@/components";
-const  NavbarList  = ({children, className}) =>{
+import { cn } from "@/helper/cn";
+import React from "react";
+
+type NavbarProps = React.ComponentProps<'nav'>
+type NavbarListProps = React.ComponentProps<'ul'>
+type NavbarListItemProps = React.ComponentProps<'li'>
+
+const  NavbarList  = ({children, className, ...props}: NavbarListProps) =>{
   return (
-    <ul className={`my-4 border-t border-indigo-400/20 hover:border-indigo-400/40 ${className}`}>
+    <ul className={cn("my-4 border-t border-indigo-400/20 hover:border-indigo-400/40", className)} {...props}>
       {children}
     </ul>
   );
 };
-const NavbarListItem = ({children, className}) =>{
+const NavbarListItem = ({children, className, ...props}: NavbarListItemProps) =>{
   return (
-    <li className={`my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer flex gap-2 items-center hover:text-slate-100 ${className}`}>
+    <li className={cn("my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer flex gap-2 items-center hover:text-slate-100", className)} {...props}>
       {children}
     </li>
   );
 };
 
-export const Navbar = () =>{
+export const Navbar = ({className, ...props}: NavbarProps) =>{
     return(
-        <nav className="flex h-screen flex-col bg-slate-900 border-r border-indigo-400/20 hover:border-indigo-400/40 w-72 p-2 text-slate-300">
+        <nav className={cn("flex h-screen flex-col bg-slate-900 border-r border-indigo-400/20 hover:border-indigo-400/40 w-72 p-2 text-slate-300", className)} {...props}>
           <div className="flex items-center justify-center">
             <img src="https://ajc-maker.s3.sa-east-1.amazonaws.com/img/Logo+Grande+1.svg" 
                  alt="AJC Maker" 
